70ff6e800182f2f4213b9c9c19f0bf557582d5f1
keashynnaidoo <keashyn.naidoo@my.jcu.edu.au>
2020-08-27 15:34:03 +0800

    intial commits

---
+++ Prac_06/languages.py
@@ -0,0 +1,19 @@
+from Practicals.prac_06.programming_language import ProgrammingLanguage
+
+
+def main():
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+    print("The Languages are ruby, python and visual_basic")
+
+    languages = [ruby, python, visual_basic]
+
+    print("The dynamically typed languages are:")
+    for language in languages:
+        if language.is_dynamic():
+            print(language.name)
+
+
+main()
