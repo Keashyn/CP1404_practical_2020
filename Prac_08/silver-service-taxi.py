ff922128e51998d521cdbd4d801b63b77e4a092d
keashynnaidoo <keashyn.naidoo@my.jcu.edu.au>
2020-09-10 13:32:12 +0800

    intial commits

---
+++ Prac_08/silver-service-taxi.py
@@ -0,0 +1,11 @@
+from Prac_08.taxi import Taxi
+class Silver_Taxi_serivice(Taxi):
+    flagfall=4.50
+    def __init__(self,name,fuel,fanciness):
+    super().__init__(name,fuel)
+    self.fanciness = fanciness
+    self.price_per_km *= fanciness
+    def get_fare(self):
+        return self.flagfall + super().get_fare()
+    def __str__(self):
+        return "{} plus flagfall of ${:.2f}".format(super().__str__(),self.flagfall)
