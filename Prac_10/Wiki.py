0d02e08b1047dc9eeb82288fbd12ea0421078760
keashynnaidoo <keashyn.naidoo@my.jcu.edu.au>
2020-09-24 12:44:12 +0800

    intial commits

---
+++ Prac_10/Wiki.py
@@ -0,0 +1,13 @@
+import wikipedia
+def search_WIKIPEDIA():
+    search_phrase=input("Enter search")
+    print(wikipedia.summary(search_phrase))
+    page=wikipedia.page(search_phrase)
+    print("Page Title:",page.title)
+    print("Page URl:",page.url)
+    print("Page Content:", page.content)
+    print("Page Image:", page.images[0])
+    print("Page Link:", page.links[0])
+    search_phrase = input("Enter search: ")
+    print("Thank you!")
+search_WIKIPEDIA()
